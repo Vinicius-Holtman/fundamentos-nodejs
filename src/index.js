6bcc02315b00dc77214304130300bd51908a64bb
Vinicius <viniholtman28@gmail.com>
2022-04-28 11:33:44 -0300

    Created reoutes with methods http

--- src/index.js
+++ src/index.js
@@ -2,8 +2,44 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Enviado"})
+app.get("/courses", (req, res) => {
+  return res.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ])
+})
+
+app.post("/courses", (req, res) => {
+  return res.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ])
+})
+
+app.put("/courses/:id", (req, res) => {
+  return res.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ])
+})
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ])
+})
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ])
 })
 
 app.listen(3000, () => console.log("Server is running!"))
